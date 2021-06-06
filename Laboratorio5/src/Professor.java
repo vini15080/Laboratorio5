32ab1e7569b4ce5a7804941a4758ff5ea314d00d
vini15080 <80992802+vini15080@users.noreply.github.com>
2021-06-06 19:07:12 -0300

    Add files via upload

---
+++ Laboratorio5/src/Professor.java
@@ -0,0 +1,60 @@
+import java.util.Date;
+import javax.persistence.Entity;
+
+@Entity
+public class Professor {
+	@Id @GeneratedValue
+	Integer id;
+	String matricula;
+	String formacao;
+	Date dataNasc;
+	
+	public Professor(Integer id, String matricula, String formacao, Date dataNasc) {
+		super();
+		this.id = id;
+		this.matricula = matricula;
+		this.formacao = formacao;
+		this.dataNasc = dataNasc;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getFormacao() {
+		return formacao;
+	}
+
+	public void setFormacao(String formacao) {
+		this.formacao = formacao;
+	}
+
+	public Date getDataNasc() {
+		return dataNasc;
+	}
+
+	public void setDataNasc(Date dataNasc) {
+		this.dataNasc = dataNasc;
+	}
+
+	@Override
+	public String toString() {
+		return "Professor [id=" + id + ", matricula=" + matricula + ", formacao=" + formacao + ", dataNasc=" + dataNasc
+				+ "]";
+	}
+	
+	
+
+}
